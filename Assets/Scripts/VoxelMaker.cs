7cd427acc0b5f6d9b1330e9f91f93475d4ce7bf9
Sodam <Theka624@gmail.com>
2025-09-22 11:11:15 +0900

    4주차 진행

--- Assets/Scripts/VoxelMaker.cs
+++ Assets/Scripts/VoxelMaker.cs
@@ -9,12 +9,20 @@ public class VoxelMaker : MonoBehaviour
     public int voxelPoolSize = 20;
     public static List<GameObject> voxelPool = new List<GameObject>();
 
+    public float createTime = 0.1f;
+    float currentTime = 0;
+
+    public Transform crosshair;
+
     // Start is called before the first frame update
     void Start()
     {
         for (int i = 0; i < voxelPoolSize; i++)
         {
             GameObject voxel = Instantiate(voxelFactory);
+
+            MeshRenderer Render = voxel.GetComponent<MeshRenderer>();
+            Render.material.color = Random.ColorHSV();
 
             voxel.SetActive(false);
 
@@ -25,26 +33,36 @@ public class VoxelMaker : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
-        if (Input.GetButtonDown("Fire1"))
-        {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hitInfo = new RaycastHit();
+    {   
+        ARAVRInput.DrawCrosshair(crosshair);
 
-            if (Physics.Raycast(ray, out hitInfo)) 
+        if (ARAVRInput.Get(ARAVRInput.Button.One))
+        {   
+            currentTime += Time.deltaTime;
+            if (currentTime > createTime)
             {
-                if (voxelPool.Count > 0 ) //오브젝트 풀 안에 복셀이 있는지 없는지 확인!           
+                //Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                Ray ray = new Ray (ARAVRInput.RHandPosition, ARAVRInput.RHandDirection);
+                RaycastHit hitInfo = new RaycastHit();
+
+                if (Physics.Raycast(ray, out hitInfo)) 
                 {
-                    GameObject voxel = voxelPool[0]; //오브젝트 풀 최상단의 값을 가져옴
+                    if (voxelPool.Count > 0 ) //오브젝트 풀 안에 복셀이 있는지 없는지 확인!           
+                    {
+                        GameObject voxel = voxelPool[0]; //오브젝트 풀 최상단의 값을 가져옴
 
-                    voxel.SetActive(true);           //객체를 활성화 함
+                        voxel.SetActive(true);           //객체를 활성화 함
 
-                    voxel.transform.position = hitInfo.point; //레이캐스트를 통해 얻은 충돌 지점의 위치로 객체를 이동
+                        voxel.transform.position = hitInfo.point; //레이캐스트를 통해 얻은 충돌 지점의 위치로 객체를 이동
 
-                    voxelPool.RemoveAt(0);           //오브젝트 풀에서 복셀 한개 제거
+                        voxelPool.RemoveAt(0);           //오브젝트 풀에서 복셀 한개 제거
+
+                        currentTime = 0;
+                    }
+                    
                 }
-                
             }
+            
         }
     }
 }
